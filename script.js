3b9935cdf5e387379231de8eac3f9edf5ddb46a8
Azamat-div <73755645+Azamat-div@users.noreply.github.com>
2025-05-17 17:27:15 +0500

    Splitting app into the JS modules

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-const quotes = [
-  {
-    quote: 'The only way to do great work is to love what you do',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: 'Innovation distinguishes between a leader and a follower',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: "Your time is limited, so don't waste it living someone else's life",
-    author: 'Steve Jobs',
-  },
-];
+import all from "./quotes.js";
 
 const quoteElement = document.getElementById('quote');
 const quoteAuthorElement = document.getElementById('quote-author');
 const generateBtn = document.getElementById('generate-btn');
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  const randomIndex = Math.floor(Math.random() * all.length);
+  const randomQuote = all[randomIndex];
   const { quote, author } = randomQuote;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;

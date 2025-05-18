131163efb93006ac16af495b35895089aafca837
Azamat-div <73755645+Azamat-div@users.noreply.github.com>
2025-05-19 02:21:13 +0500

    Add quotes.js; update style.css, script.js, and index.htmlx.html

--- quotes.js
+++ quotes.js
@@ -2,14 +2,17 @@ const quotes = [
 {
     quote: 'The only way to do great work is to love what you do',
     author: 'Steve Jobs',
+    isFavorite: false,
 },
 {
     quote: 'Innovation distinguishes between a leader and a follower',
     author: 'Steve Jobs',
+    isFavorite: false,
 },
 {
     quote: "Your time is limited, so don't waste it living someone else's life",
     author: 'Steve Jobs',
+    isFavorite: false,
 },
 ];
 

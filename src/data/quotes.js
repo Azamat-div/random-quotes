993009e2f5ed70e66eea1f11004774a29695e062
Azamat-div <73755645+Azamat-div@users.noreply.github.com>
2025-05-23 13:53:17 +0500

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+{
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+    isFavorite: false,
+},
+{
+    text: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+    isFavorite: false,
+},
+{
+    text: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+    isFavorite: false,
+},
+];
+
+export default quotes;

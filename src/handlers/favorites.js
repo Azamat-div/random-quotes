1862157d11060890cf7a19adbc8366a2ea259a40
Azamat-div <73755645+Azamat-div@users.noreply.github.com>
2025-05-27 01:52:16 +0500

    Add quote id to each favorite quote card and remove card from the DOM by id

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -1,52 +1,51 @@
 import { favoriteBtn } from '../../index.js';
 
 function toggleFavorite(quote, btn, container) {
-    quote.isFavorite = !quote.isFavorite;
-    const { text, author, isFavorite } = quote;
-    toggleFavoriteBtnIcon(isFavorite, btn);
+  quote.isFavorite = !quote.isFavorite;
+  toggleFavoriteBtnIcon(quote.isFavorite, btn);
 
-    if (isFavorite) {
-        showFavoriteCard(text, author, container);
-    } else {
-        hideFavoriteCard(text);
-    }
+  quote.isFavorite ? showFavoriteCard(quote, container) : hideFavoriteCard(quote.id)
+
+  if (quote.isFavorite) {
+      showFavoriteCard(quote, container);
+  } else {
+      hideFavoriteCard(quote.id);
+  }
 }
 
 function handleFavorite(isFavorite) {
-    showFavoriteBtn(favoriteBtn);
-    toggleFavoriteBtnIcon(isFavorite, favoriteBtn); 
+  showFavoriteBtn(favoriteBtn);
+  toggleFavoriteBtnIcon(isFavorite, favoriteBtn); 
 };
 
 function toggleFavoriteBtnIcon(isFavorite, el) {
-    el.classList.toggle('fa', isFavorite);
-    el.classList.toggle('far', !isFavorite);
+  el.classList.toggle('fa', isFavorite);
+  el.classList.toggle('far', !isFavorite);
 }
 
 function showFavoriteBtn(btn) {
-    btn.style.display = 'inline-block';
+  btn.style.display = 'inline-block';
 }
 
 function hideFavoriteBtn(btn) {
-    btn.style.display = 'none';
+  btn.style.display = 'none';
 }
 
-function showFavoriteCard(text, author, container) {
-    const favoriteCard = document.createElement('div');
-    favoriteCard.classList.add('favorite-card');
-    favoriteCard.innerHTML = `
-    <p>${text}</p>
-    <p class="author">${author}</p>
+function showFavoriteCard(quote, container) {
+  const { id, text, author } = quote;
+  const favoriteCard = document.createElement('div');
+  favoriteCard.classList.add('favorite-card');
+  favoriteCard.dataset.quoteId = id;
+  favoriteCard.innerHTML = `
+  <p>${text}</p>
+  <p class="author">${author}</p>
 `;
-    container.appendChild(favoriteCard);
+  container.appendChild(favoriteCard);
 }
 
-function hideFavoriteCard(text) {
-    const favoriteCards = document.querySelectorAll('.favorite-card');
-    favoriteCards.forEach(card => {
-    if (card.textContent.includes(text)) {
-        card.remove();
-    }
-});
+function hideFavoriteCard(id) {
+  const card = document.querySelector(`.favorite-card[data-quote-id="${id}"]`)
+  if (card) card.remove();
 }
 
 export { handleFavorite, toggleFavorite, hideFavoriteBtn };
